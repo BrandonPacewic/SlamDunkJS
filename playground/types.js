cc5d2213bcaebcc0bf81d29af33186a81103e30b
Brandon <pacewicbrandon@gmail.com>
2021-12-10 15:09:32 -0800

    update to type test

--- playground/types.js
+++ playground/types.js
@@ -1,3 +1,23 @@
 'use strict';
 
-const 
+const char = 'a';
+const num = 100;
+
+const me = {
+    age: 16,
+    name: 'Brandon',
+    pet: 'Leo'
+};
+
+console.log(me);
+
+document.body.innerHTML = `
+    <body>
+        <div>
+            <h3> ${Math.round(Math.random() * 1000)} </h3>
+        </div>
+
+        <h2>${me.name}</h2>
+        <h3>${me.age}, ${me.pet}</h3>
+    </body>
+`
