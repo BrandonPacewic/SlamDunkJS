00b456ada137ff8705e4767f4212a1ceabb3ed90
Brandon <pacewicbrandon@gmail.com>
2021-12-10 12:04:22 -0800

    file save

--- playground/html-string-test.js
+++ playground/html-string-test.js
@@ -4,7 +4,15 @@ let string = '';
 const times = 5;
 
 for (let i = 0; i < times; i++) {
-    string += i + '<br></br>';
+    string += i + ' cool' + '<br></br>';
 }
 
-console.log(string);
+const html = `
+    <body>
+        <div>
+            <h2> ${string} </h2>
+        </div<
+    </body>
+`
+
+document.body.innerHTML = html;
