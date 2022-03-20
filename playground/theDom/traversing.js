3d39085d6627ee800028f716e3eac3e7824128b2
Brandon <pacewicbrandon@gmail.com>
2022-03-19 18:36:54 -0700

    traversing

---
+++ playground/theDom/traversing.js
@@ -0,0 +1,12 @@
+console.log('it works');
+const wesItem = document.querySelector('.wes');
+console.log('this is the first ', wesItem.childNodes);
+console.log('this is the second', wesItem.firstChild);
+console.log('this is the third', wesItem.lastChild);
+console.log('this is the fourth', wesItem.children);
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.append(p);
+p.remove();
+// Element still exists in code just not the dom
+console.log(p);
